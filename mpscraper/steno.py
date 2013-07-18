6582f7bf2d46c9623efe7bb4bdca75e7c1611e80
Alex Morega <alex@grep.ro>
2013-07-18 14:05:20 +0300

    fetch a particular page

---
+++ mpscraper/steno.py
@@ -0,0 +1,20 @@
+""" Fetch and parse stenograms """
+
+from datetime import date
+import requests
+
+
+def fetch_day(day):
+    contents_page = requests.get(
+        'http://www.cdep.ro/pls/steno/steno.data',
+        params={
+            'cam': 2,
+            'dat': day.strftime('%Y%m%d'),
+            'idl': 1,
+        })
+
+    print contents_page.text
+
+
+if __name__ == '__main__':
+    fetch_day(date(2013, 6, 10))
