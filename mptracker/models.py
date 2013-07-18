f29bce5539a82e17ad229c54d7d156c0e473fe5f
Alex Morega <alex@grep.ro>
2013-07-18 19:46:13 +0300

    Person model

--- mptracker/models.py
+++ mptracker/models.py
@@ -1,4 +1,15 @@
+import uuid
 from flask.ext.sqlalchemy import SQLAlchemy
+
+def uuid_column():
+    return db.Column(db.CHAR(32), primary_key=True,
+                     default=lambda: str(uuid.uuid4()))
 
 
 db = SQLAlchemy()
+
+
+class Person(db.Model):
+    id = uuid_column()
+    name = db.Column(db.String)
+    cdep_id = db.Column(db.Integer)
