b88cd6a95bcc04615119e9ec1b6623befb006de1
Alex Morega <alex@grep.ro>
2013-07-18 14:25:45 +0300

    extract link to stenogram pages

--- mpscraper/steno.py
+++ mpscraper/steno.py
@@ -1,20 +1,23 @@
 """ Fetch and parse stenograms """
 
 from datetime import date
+from urlparse import urlparse, parse_qs
 from path import path
-import requests
+from pyquery import PyQuery as pq
 
 
 def fetch_day(day):
-    contents_page = requests.get(
-        'http://www.cdep.ro/pls/steno/steno.data',
-        params={
-            'cam': 2,
-            'dat': day.strftime('%Y%m%d'),
-            'idl': 1,
-        })
-
-    print contents_page.text
+    contents = pq('http://www.cdep.ro/pls/steno/steno.data',
+                  {'cam': 2, 'dat': day.strftime('%Y%m%d'), 'idl': 1})
+    for link_el in contents('td.headlinetext1 b a'):
+        link = link_el.attrib['href']
+        plink = urlparse(link)
+        assert plink.path == '/pls/steno/steno.stenograma'
+        if ',' in parse_qs(plink.query)['idm'][0]:
+            # this is a fragment page. we can ignore it since we
+            # already have the content from the parent page.
+            continue
+        print link
 
 
 if __name__ == '__main__':
