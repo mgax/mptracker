85e36cf69c411cd22f98c9390e4489a3114d0881
Alex Morega <alex@grep.ro>
2013-07-18 21:27:28 +0300

    page for each person

--- mptracker/pages.py
+++ mptracker/pages.py
@@ -9,3 +9,9 @@ pages = flask.Blueprint('pages', __name__)
 def home():
     people = models.Person.query.order_by('name')
     return flask.render_template('home.html', people=people)
+
+
+@pages.route('/person/<person_id>')
+def person(person_id):
+    person = models.Person.query.get_or_404(person_id)
+    return flask.render_template('person.html', person=person)
