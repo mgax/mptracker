43b5f337cc02b2befe82dd0b1e9c7657a88cfb70
Alex Morega <alex@grep.ro>
2013-07-30 16:00:41 +0300

    log errors to sentry

--- mptracker/app.py
+++ mptracker/app.py
@@ -17,6 +17,10 @@ def configure(app):
     db_path = data_dir / 'db.sqlite'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
     app.debug = (os.environ.get('DEBUG') == 'on')
+    sentry_dsn = os.environ.get('SENTRY_DSN')
+    if sentry_dsn:
+        from raven.contrib.flask import Sentry
+        Sentry(app, dsn=sentry_dsn)
 
 
 def create_app():
