03dbdba101688c3ff281e97370605648d3e90300
Alex Morega <alex@grep.ro>
2013-08-02 21:45:11 +0300

    more tables; ability to search

--- mptracker/admin.py
+++ mptracker/admin.py
@@ -3,6 +3,13 @@ from flask.ext.admin.contrib.sqla import ModelView
 from mptracker import models
 
 
+class QuestionView(ModelView):
+
+    column_searchable_list = ['title']
+
+
 admin = Admin(name="MP Tracker")
 admin.add_view(ModelView(models.Person, models.db.session))
-admin.add_view(ModelView(models.Question, models.db.session))
+admin.add_view(QuestionView(models.Question, models.db.session))
+admin.add_view(ModelView(models.StenoChapter, models.db.session))
+admin.add_view(ModelView(models.StenoParagraph, models.db.session))
