d43846d880c5d87df89455401cbfd2bdba7f6185
Alex Morega <alex@grep.ro>
2013-08-02 20:54:15 +0300

    save questions from csv to database

--- mptracker/questions.py
+++ mptracker/questions.py
@@ -1,13 +1,21 @@
 import csv
 import flask
 from flask.ext.script import Manager
+from mptracker import models
 
 
 questions_manager = Manager()
 
 @questions_manager.command
 def load(csv_path):
+    person_matcher = models.PersonMatcher()
     with open(csv_path, 'r') as csv_file:
         csv_doc = csv.DictReader(csv_file)
         for row in csv_doc:
-            print(row['person_cdep_id'])
+            person = person_matcher.get_person(row.pop('person_name'),
+                                               int(row.pop('person_cdep_id')),
+                                               strict=True)
+            row['date'] = models.parse_date(row.pop('date'))
+            question = models.Question(person=person, **row)
+            models.db.session.add(question)
+    models.db.session.commit()
