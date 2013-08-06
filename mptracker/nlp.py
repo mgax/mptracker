b7a97d8a01a3435b6a27f345ee45af63c7ff8f99
Alex Morega <alex@grep.ro>
2013-08-06 20:29:56 +0300

    reimplement tokenizer using regular expression

--- mptracker/nlp.py
+++ mptracker/nlp.py
@@ -1,10 +1,18 @@
 from collections import namedtuple
+import re
 
 Token = namedtuple('Token', ['text'])
+word_pattern = re.compile(r'\S+')
 
 
 def tokenize(text):
-    for word in text.split():
+    offset = 0
+    while True:
+        match = word_pattern.search(text, offset)
+        if match is None:
+            break
+        word = match.group()
+        offset = match.end()
         word = word.strip(',.;!?-()')
         if word:
             yield Token(word)
