5e17e27a23cb7bd2ca9bf00476e052d4f3aa89aa
Alex Morega <alex@grep.ro>
2013-08-08 17:50:51 +0300

    remove old csv importer

--- mptracker/questions.py
+++ mptracker/questions.py
@@ -18,49 +18,6 @@ questions = flask.Blueprint('questions', __name__)
 
 questions_manager = Manager()
 
-@questions_manager.command
-def load(csv_path):
-    person_matcher = models.PersonMatcher()
-
-    existing = {(q.number, q.date): q for q in models.Question.query}
-
-    n_add = n_update = n_ok = 0
-    with open(csv_path, 'r') as csv_file:
-        csv_doc = csv.DictReader(csv_file)
-        for row in csv_doc:
-            row['date'] = models.parse_date(row.pop('date'))
-            person = person_matcher.get_person(row.pop('person_name'),
-                                               int(row.pop('person_cdep_id')),
-                                               strict=True)
-            row['person'] = person
-
-            if (row['number'], row['date']) in existing:
-                question = existing[row['number'], row['date']]
-                changed = False
-                for k in row:
-                    if getattr(question, k) != row[k]:
-                        setattr(question, k, row[k])
-                        changed = True
-
-                if not changed:
-                    n_ok += 1
-                    continue
-
-                n_update += 1
-                logger.info("Updating question %s/%s",
-                            row['number'], row['date'])
-
-            else:
-                question = models.Question(**row)
-                n_add += 1
-                logger.info("Adding question %s/%s",
-                            row['number'], row['date'])
-
-            models.db.session.add(question)
-
-    models.db.session.commit()
-    logger.info("Created %d, updated %d, found ok %d.", n_add, n_update, n_ok)
-
 
 @job
 def ocr_question(question_id):
