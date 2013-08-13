552035a435670d5349cdad6889ee2a300ce4e6a6
Alex Morega <alex@grep.ro>
2013-08-13 20:13:20 +0300

    use the TablePatcher class

--- mptracker/scraper/__init__.py
+++ mptracker/scraper/__init__.py
@@ -14,44 +14,32 @@ scraper_manager = Manager()
 def questions():
     from mptracker.scraper.questions import QuestionScraper
 
-    existing = {(q.number, q.date): q for q in models.Question.query}
+    patcher = TablePatcher(models.Question,
+                           models.db.session,
+                           key_columns=['number', 'date'])
+
     person_matcher = models.PersonMatcher()
-    n_add = n_update = n_ok = 0
 
-    questions_scraper = QuestionScraper(get_cached_session())
-    for question in questions_scraper.run():
-        person = person_matcher.get_person(question.person_name,
-                                           question.person_cdep_id,
-                                           strict=True)
-        q_data = {
-            'number':    question.number,
-            'type':      question.q_type,
-            'method':    question.method,
-            'title':     question.title,
-            'url':       question.url,
-            'pdf_url':   question.pdf_url,
-            'addressee': '; '.join(question.addressee),
-            'date':      question.date,
-            'person':    person,
-        }
+    def get_questions():
+        questions_scraper = QuestionScraper(get_cached_session())
+        for question in questions_scraper.run():
+            person = person_matcher.get_person(question.person_name,
+                                               question.person_cdep_id,
+                                               strict=True)
+            q_data = {
+                'number':    question.number,
+                'type':      question.q_type,
+                'method':    question.method,
+                'title':     question.title,
+                'url':       question.url,
+                'pdf_url':   question.pdf_url,
+                'addressee': '; '.join(question.addressee),
+                'date':      question.date,
+                'person_id': person.id,
+            }
+            yield q_data
 
-        row = existing[question.number, question.date]
-
-        changed = False
-        for k, v in q_data.items():
-            if getattr(row, k) != v:
-                setattr(row, k, v)
-                changed = True
-
-        if changed:
-            logger.info('changed %s', row.id)
-            n_update += 1
-
-        else:
-            n_ok += 1
-
-    logger.info("Created %d, updated %d, found ok %d.", n_add, n_update, n_ok)
-    models.db.session.commit()
+    patcher.update(get_questions())
 
 
 @scraper_manager.command
