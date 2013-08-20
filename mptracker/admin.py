aa0143da1f83b2f7ed16a5e6a3313e5c9a05ba59
Alex Morega <alex@grep.ro>
2013-08-20 11:16:28 +0300

    join our ModelView subclasses into single class

--- mptracker/admin.py
+++ mptracker/admin.py
@@ -1,37 +1,45 @@
+import flask
 from flask.ext.admin import Admin
-from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.admin.contrib.sqla import ModelView as ModelView_base
 from mptracker import models
 from mptracker.auth import is_privileged
 
 admin = Admin(name="MP Tracker")
 
 
-class AuthView(ModelView):
+class ModelView(ModelView_base):
 
     can_create = False
     can_edit = False
     can_delete = False
 
+    list_template = 'questions/admin_list.html'
+    view_route = None
+
+    def __init__(self, model, **kwargs):
+        super().__init__(model, models.db.session)
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
     def is_accessible(self):
         return is_privileged()
 
-admin.add_view(AuthView(models.StenoChapter, models.db.session))
-admin.add_view(AuthView(models.StenoParagraph, models.db.session))
-admin.add_view(AuthView(models.User, models.db.session))
-admin.add_view(AuthView(models.County, models.db.session))
-admin.add_view(AuthView(models.Proposal, models.db.session))
-admin.add_view(AuthView(models.CommitteeSummary, models.db.session))
+    def get_view_url(self, row):
+        if self.view_route:
+            (view_name, pk_name) = self.view_route.split(':')
+            return flask.url_for(view_name, **{pk_name: row.id})
+
+        else:
+            return None
 
 
-class PersonView(AuthView):
-
-    column_searchable_list = ['name']
-
-admin.add_view(PersonView(models.Person, models.db.session))
-
-
-class QuestionView(AuthView):
-
-    list_template = 'questions/admin_list.html'
-
-admin.add_view(QuestionView(models.Question, models.db.session))
+admin.add_view(ModelView(models.StenoChapter))
+admin.add_view(ModelView(models.StenoParagraph))
+admin.add_view(ModelView(models.User))
+admin.add_view(ModelView(models.County))
+admin.add_view(ModelView(models.Proposal))
+admin.add_view(ModelView(models.CommitteeSummary))
+admin.add_view(ModelView(models.Question,
+                         view_route='questions.question_detail:question_id'))
+admin.add_view(ModelView(models.Person,
+                         column_searchable_list=['name']))
