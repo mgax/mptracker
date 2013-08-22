3395d044f49e445b7e586c009535efd061c44cf6
Alex Morega <alex@grep.ro>
2013-08-22 14:59:04 +0300

    serve csv with proper content type (#8)

--- mptracker/questions.py
+++ mptracker/questions.py
@@ -128,7 +128,7 @@ def person_questions(person_id):
         cols = ['id', 'title', 'date', 'is_local_topic_flag',
                 'score', 'addressee']
         return flask.Response(csv_lines(cols, questions),
-                              mimetype='text/plain')
+                              mimetype='text/csv')
 
     addressee_top = sorted(((n, name) for name, n in addressee_count.items()),
                            reverse=True)[:5]
