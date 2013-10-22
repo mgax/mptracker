f20e99f085825a7a00f8cde84651d074a2682365
Alex Morega <alex@grep.ro>
2013-10-22 14:41:25 +0300

    install postgresql 9.3

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+POSTGRES_KEY_URL='http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc'
+POSTGRES_REPO='deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main'
+
 export DEBIAN_FRONTEND=noninteractive
 
+apt-get install -y curl python-software-properties
+curl "$POSTGRES_KEY_URL" | sudo apt-key add -
+add-apt-repository -y "$POSTGRES_REPO"
 apt-get update
 apt-get upgrade -y
+apt-get install -y postgresql-9.3
