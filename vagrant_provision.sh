da92bc9847cba814e5e008ae4762b4b7607ecb61
Alex Morega <alex@grep.ro>
2013-10-22 15:31:23 +0300

    install python requirements

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -15,7 +15,9 @@ add-apt-repository -y ppa:fkrull/deadsnakes
 apt-get update
 apt-get upgrade -y
 apt-get install -y postgresql-9.3 postgresql-contrib-9.3 \
-                   build-essential python3.3 python3.3-dev
+                   postgresql-server-dev-9.3 \
+                   build-essential libxml2-dev libxslt1-dev \
+                   python3.3 python3.3-dev git
 
 sudo -u postgres psql -c "CREATE USER mptracker WITH ENCRYPTED PASSWORD 'mptracker';"
 sudo -u postgres psql -c "CREATE DATABASE mptracker"
@@ -28,3 +30,5 @@ if [ ! -d py33env ]; then
     sudo -u vagrant curl -O "$PIP_URL"
     sudo -u vagrant python virtualenv.py -p python3.3 py33env
 fi
+
+sudo -u vagrant py33env/bin/pip install -r /vagrant/requirements-dev.txt
