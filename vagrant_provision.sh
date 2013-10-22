3f649ab911b8f25872a4ebcb307f398715ab41b2
Alex Morega <alex@grep.ro>
2013-10-22 15:04:09 +0300

    create user and db

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -13,3 +13,7 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y postgresql-9.3 postgresql-contrib-9.3 \
                    build-essential python3.3 python3.3-dev
+
+sudo -u postgres psql -c "CREATE USER mptracker WITH ENCRYPTED PASSWORD 'mptracker';"
+sudo -u postgres psql -c "CREATE DATABASE mptracker"
+sudo -u postgres psql mptracker -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'
