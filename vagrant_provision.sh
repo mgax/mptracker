99b60f5ab29c96984f86af796e3858caf59791ec
Alex Morega <alex@grep.ro>
2013-10-22 15:19:23 +0300

    create a virtualenv

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -2,6 +2,9 @@
 
 POSTGRES_KEY_URL='http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc'
 POSTGRES_REPO='deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main'
+VIRTUALENV_PY='https://raw.github.com/pypa/virtualenv/develop/virtualenv.py'
+SETUPTOOLS_URL='https://pypi.python.org/packages/source/s/setuptools/setuptools-1.1.6.tar.gz'
+PIP_URL='https://pypi.python.org/packages/source/p/pip/pip-1.4.1.tar.gz'
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -17,3 +20,11 @@ apt-get install -y postgresql-9.3 postgresql-contrib-9.3 \
 sudo -u postgres psql -c "CREATE USER mptracker WITH ENCRYPTED PASSWORD 'mptracker';"
 sudo -u postgres psql -c "CREATE DATABASE mptracker"
 sudo -u postgres psql mptracker -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'
+
+cd /home/vagrant
+if [ ! -d py33env ]; then
+    sudo -u vagrant curl -O "$VIRTUALENV_PY"
+    sudo -u vagrant curl -O "$SETUPTOOLS_URL"
+    sudo -u vagrant curl -O "$PIP_URL"
+    sudo -u vagrant python virtualenv.py -p python3.3 py33env
+fi
