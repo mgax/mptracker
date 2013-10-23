b002b9e21e5a3b295271f89464f317adc692d08c
Alex Morega <alex@grep.ro>
2013-10-23 09:51:01 +0300

    reorganize installation of packages

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -6,18 +6,17 @@ VIRTUALENV_PY='https://raw.github.com/pypa/virtualenv/develop/virtualenv.py'
 SETUPTOOLS_URL='https://pypi.python.org/packages/source/s/setuptools/setuptools-1.1.6.tar.gz'
 PIP_URL='https://pypi.python.org/packages/source/p/pip/pip-1.4.1.tar.gz'
 
-export DEBIAN_FRONTEND=noninteractive
-
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 sudo apt-get install -y curl python-software-properties
 sudo curl "$POSTGRES_KEY_URL" | sudo apt-key add -
 sudo add-apt-repository -y "$POSTGRES_REPO"
 sudo add-apt-repository -y ppa:fkrull/deadsnakes
 sudo apt-get update
-sudo apt-get upgrade -y
 sudo apt-get install -y postgresql-9.3 postgresql-contrib-9.3 \
                         postgresql-server-dev-9.3 \
                         build-essential libxml2-dev libxslt1-dev \
-                        python3.3 python3.3-dev git
+                        python3.3 python3.3-dev git vim
 
 sudo -u postgres psql -c "CREATE USER mptracker WITH ENCRYPTED PASSWORD 'mptracker';"
 sudo -u postgres psql -c "CREATE DATABASE mptracker"
