9244fa24eef36832278bd2cc7dd387d0cd398f9e
Alex Morega <alex@grep.ro>
2013-11-21 14:45:17 +0200

    nav bar

--- mptracker/website/pages.py
+++ mptracker/website/pages.py
@@ -4,6 +4,26 @@ from mptracker import models
 pages = flask.Blueprint('pages', __name__)
 
 
+@pages.app_context_processor
+def inject_nav_links():
+    return {
+        'nav_link_list': [
+            dict(
+                url=flask.url_for('.person_index'),
+                label="Deputați",
+            ),
+            dict(
+                url=flask.url_for('.party_index'),
+                label="Partide",
+            ),
+            dict(
+                url=flask.url_for('.policy_index'),
+                label="Domenii de politici publice",
+            ),
+        ],
+    }
+
+
 @pages.route('/_crashme', methods=['GET', 'POST'])
 def crashme():
     if flask.request.method == 'POST':
@@ -21,3 +41,18 @@ def ping():
 @pages.route('/')
 def home():
     return flask.render_template('home.html')
+
+
+@pages.route('/persoane/')
+def person_index():
+    return flask.render_template('layout.html')
+
+
+@pages.route('/partide/')
+def party_index():
+    return flask.render_template('layout.html')
+
+
+@pages.route('/politici/')
+def policy_index():
+    return flask.render_template('layout.html')
