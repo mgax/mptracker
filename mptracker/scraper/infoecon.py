63949492df0fcfc7ccde98703bc97994d019371f
alexandrupetrescu94 <alexandrupetrescu94@yahoo.com>
2013-11-21 15:21:25 +0200

    Almost finished scraper

--- mptracker/scraper/infoecon.py
+++ mptracker/scraper/infoecon.py
@@ -7,7 +7,6 @@ from mptracker.scraper.common import (Scraper, url_args, GenericModel,
 class EconScraper(Scraper):
 		index_url = 'http://www.cdep.ro/pls/parlam/informatii_economice.home'
 
-
 		def fetch(self):
 			index_page=self.fetch_url(self.index_url)
 			headline=index_page.find('#signup')
@@ -18,7 +17,7 @@ class EconScraper(Scraper):
 			for link in headline_tables_tr.items('td > a'):
 				url_set.add(link.attr('href'))
 			for url in url_set:
-				return self.fetch_section(url) #Test cu return, a se modifica cu la final @ yield self.fetch_section(url)   
+				return self.fetch_section(url) #Tested with return, when finished @ yield self.fetch_section(url)   
 
 
 		def fetch_section(self, section_url):
@@ -30,7 +29,7 @@ class EconScraper(Scraper):
 				items.append(item.split('='))
 
 			if (items[1][1]<'2010' or (items[1][1]=='2010' and items[2][1]<='s3') ):
-				return None #Pentru tot ce este mai mic sau egal cu leg=2008&an=2010&lu=3 nu exista legatura catre sectiunea D
+				return None #For everything that is <= leg=2008&an=2010&lu=3 section D is not present
 			else:
 				section_page = self.fetch_url(section_url)
 				headline = section_page.find('#pageContent')
@@ -40,31 +39,36 @@ class EconScraper(Scraper):
 				url_set = set()
 				url_set.add(mp_table.attr('href'))
 				for url in url_set:
-					return self.fetch_table(url) #Vezi comment line 20
+					return self.fetch_table(url) #See comment line 20
 
 		def fetch_table(self,table_url):
 			print(table_url)
 			table_page = self.fetch_url(table_url)
-			table_headline = table_page.find('.rowh') #Dictionary Key's
-
-			data=dict()
+			#tr bgcolor (delimiter) -> tr row0,tr row1
+			
+			#headline_keys stored on @data array
+			table_headline = table_page.find('.rowh')
+			data=[]
 			table=table_headline.children()
 			for item in table.items():
-				data[item.text()]=None
-			OrderedDict(data)
-			for item in table.items():
-				print (item.text().encode('utf-8'))
-			'''
-			De genul asta:
+				data.append(item.text())
+			#	print (item.text().encode('utf-8'))
+			
+			#Dict structure:
+			#	Key=Circumscriptie
+			#	List of td's returned with the headline names from @data array
 
-			dict({
-				nr.headline.key: nr
-				circumscr/coleg.key: [circumsc,coleg]
-				chelt.key: chelt
-				cheltbun.key: chelt
-				})
-			'''	
+			c=OrderedDict()
+			table_trs=pq(table_headline).siblings()
+			for item in table_trs.items():
+				if (item.hasClass('row0') or item.hasClass('row1')):
+					c[key]=[]
+					#print (key)
+					for td in item.find('td').items():
+					#	print (td.text().encode('utf-8'))
+						c[key].append(td.text().encode('utf-8'))
+				else:
+					key=(item.text().encode('utf-8'))
 
-			table_trs = pq(table_headline).siblings()
-			table_items = list(table_trs.items())
-			#tr bgcolor (delimiter) -> tr row0,tr row1
+			for key,value in c.items():
+				print (key,value)
