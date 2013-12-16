5a8b7e1d3caf87551b567bc8b8b2aa10b5a4aea2
Alex Morega <alex@grep.ro>
2013-12-16 14:20:26 +0200

    reorder migrations

--- alembic/versions/1b8378b8914_person_romania_curat.py
+++ alembic/versions/1b8378b8914_person_romania_curat.py
@@ -1,14 +1,14 @@
 revision = '1b8378b8914'
-down_revision = '32590709513'
+down_revision = '55c008192aa'
 
 from alembic import op
 import sqlalchemy as sa
 
 
 def upgrade():
-    op.add_column('person', 
+    op.add_column('person',
             sa.Column('romania_curata', sa.Text(), nullable=True))
-    
+
 
 def downgrade():
     op.drop_column('person', 'romania_curata')
