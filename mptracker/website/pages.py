f341518e7eb88d9efada4c53e9b1dc202b8f07fd
Alex Morega <alex@grep.ro>
2014-06-20 16:01:12 +0300

    text for policy page

--- mptracker/website/pages.py
+++ mptracker/website/pages.py
@@ -80,9 +80,16 @@ def inject_nav_links():
     }
 
 
-def template_text(name, fold=False):
+def template_text(name, fold=False, below_fold=False):
     html = flask.render_template('text_%s.html' % name, layout=False)
-    return jinja2.Markup(html.split('<!-- fold -->')[0] if fold else html)
+
+    if fold:
+        html = html.split('<!-- fold -->')[0]
+
+    elif below_fold:
+        html = html.split('<!-- fold -->')[1]
+
+    return jinja2.Markup(html)
 
 
 @pages.record
