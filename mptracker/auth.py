5bb5b065cdd324292c1d4a836560a2f59400a390
Alex Morega <alex@grep.ro>
2014-06-25 13:30:04 +0300

    drop flask-browserid

--- mptracker/auth.py
+++ mptracker/auth.py
@@ -1,12 +1,9 @@
 from functools import wraps
 import flask
 from flask.ext.login import LoginManager, current_user
-from flask.ext.browserid import BrowserID
 from mptracker import models
 
 login_manager = LoginManager()
-
-browser_id = BrowserID()
 
 auth = flask.Blueprint('auth', __name__)
 
@@ -16,16 +13,9 @@ def get_user_by_id(user_id):
     return models.User.query.get(user_id)
 
 
-@browser_id.user_loader
-def get_user(user_data):
-    assert user_data['status'] == 'okay'
-    return models.User.get_or_create(email=user_data['email'])
-
-
 @auth.record
 def register_login(state):
     login_manager.init_app(state.app)
-    browser_id.init_app(state.app)
 
 
 @auth.route('/login')
