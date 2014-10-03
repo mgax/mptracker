856ce9d8a4f5bdc49284c2c4f5a248f1ad4cfb07
Alex Morega <alex@grep.ro>
2014-10-03 16:56:40 +0300

    fix typo in scraper

--- mptracker/scraper/__init__.py
+++ mptracker/scraper/__init__.py
@@ -606,7 +606,7 @@ def get_proposal_pages(
     for record in chain(scraper.list_proposals(2, year),
                         scraper.list_proposals(1, year)):
 
-        old_date = db_page_date.get((chamber, pk))
+        old_date = db_page_date.get((record['chamber'], record['pk']))
         if old_date and old_date >= record['date']:
             continue
 
