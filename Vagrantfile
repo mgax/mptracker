4aa754110b4e8cc9007936214042a50f05e09bd6
Alex Morega <alex@grep.ro>
2020-07-22 18:02:35 +0300

    Update vagrant scripts

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,7 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "precise32"
+  config.vm.box = "hashicorp/bionic64"
   config.vm.box_url = "http://files.vagrantup.com/precise32.box"
 
   config.vm.network :forwarded_port, guest: 5000, host: 5001
--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 POSTGRES_KEY_URL='http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc'
 POSTGRES_REPO='deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main'
@@ -7,16 +7,14 @@ SETUPTOOLS_URL='https://pypi.python.org/packages/source/s/setuptools/setuptools-
 PIP_URL='https://pypi.python.org/packages/source/p/pip/pip-1.4.1.tar.gz'
 
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-sudo apt-get install -y curl python-software-properties
+sudo apt-get install -y curl
 sudo curl "$POSTGRES_KEY_URL" | sudo apt-key add -
 sudo add-apt-repository -y "$POSTGRES_REPO"
-sudo add-apt-repository -y ppa:fkrull/deadsnakes
 sudo apt-get update
 sudo apt-get install -y postgresql-9.3 postgresql-contrib-9.3 \
                         postgresql-server-dev-9.3 \
                         build-essential libxml2-dev libxslt1-dev \
-                        python3.3 python3.3-dev git vim
+                        python3-dev git vim virtualenv
 
 sudo sed -e 's/^\(local .*\) peer$/\1 trust/' -i /etc/postgresql/9.3/main/pg_hba.conf
 sudo service postgresql restart
@@ -27,18 +25,18 @@ createdb mptracker -U mptracker -E UTF8 --lc-collate=en_US.UTF-8 --lc-ctype=en_U
 psql mptracker -U mptracker -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'
 
 cd /home/vagrant
-if [ ! -d py33env ]; then
+if [ ! -d venv ]; then
     echo "Creating virtualenv ..."
     curl -O "$VIRTUALENV_PY"
     curl -O "$SETUPTOOLS_URL"
     curl -O "$PIP_URL"
-    python virtualenv.py -p python3.3 py33env
-    grep py33env .bashrc ||  echo "source ~/py33env/bin/activate" >> .bashrc
+    virtualenv -p python3 venv
+    grep venv .bashrc ||  echo "source ~/venv/bin/activate" >> .bashrc
 fi
 
 cd /vagrant
 
-~/py33env/bin/pip install -r requirements-dev.txt
+~/venv/bin/pip install -r requirements-dev.txt
 
 if [ ! -f settings.py ]; then
     echo "Creating configuration file ..."
